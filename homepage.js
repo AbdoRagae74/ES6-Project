079c9f473cc6776725452914271157a5f31d411d
AbdoRagae74 <abdelrahmanragae34@gmail.com>
2025-04-25 12:48:57 +0300

    :Res:

--- homepage.js
+++ homepage.js
@@ -356,7 +356,8 @@ document.getElementById("cart-cnt").innerText = userCart.items.length;
 
 for (var i = 1; i <= 40; i++) {
   const productCard = document.createElement('div');
-  productCard.className = 'pro';
+  // productCard.className = 'pro';
+  productCard.className = 'col-12 col-sm-6 col-md-4 col-lg-3 mb-4 pro';
   productCard.innerHTML = `
        <img src="${products[i].img}"alt="${products[i].id}" class="product-img"  > 
        <div class="des">
@@ -400,7 +401,8 @@ function changeDisplayedProducts(cat,value) {
   for (var i = 1; i <= 40; i++) {
     if ( (products[i].category == cat && products[i].price>=value ) || (cat == "All"&& products[i].price>=value)  ) {
       const productCard = document.createElement('div');
-      productCard.className = 'pro';
+      productCard.className = 'col-12 col-sm-6 col-md-4 col-lg-3 mb-4 pro';
+      // productCard.className = 'pro';
       productCard.innerHTML = `
       <img src="${products[i].img}"alt="${products[i].id}" class="product-img"  > 
       <div class="des">
